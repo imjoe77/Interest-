4aa095bd737512fc3497973a664674e9b99e78d2
Joe <nathanielbandi636@gmail.com>
2025-10-04 09:16:39 +0530

    Create SI.py

---
+++ SI.py
@@ -0,0 +1,9 @@
+principal = float(input("Enter the Principal amount: "))
+rate = float(input("Enter the Rate of Interest (%): "))
+time = float(input("Enter the Time (in years): "))
+
+# Formula: SI = (P * R * T) / 100
+simple_interest = (principal * rate * time) / 100
+
+# Output
+print("Simple Interest is:", simple_interest)
